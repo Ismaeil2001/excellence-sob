c320faa05f582ae29a0ee95ccd7755650cbd312a
Ismaeil2001 <ID245@live.mdx.ac.uk>
2021-11-21 18:57:20 +0400

    fixed grade letter errors
    I fixed the errors for finding the letter grade e.g:-("A"), by using the average of all 3 grades. I deleted some lines to make it easier.

--- broken_grades.py
+++ broken_grades.py
@@ -28,23 +28,20 @@ exam_three = int(input("Input exam grade three: "))
 grades=(exam_one + exam_two + exam_three)
 avg=grades/3
 print("Average:",avg)
-sum = 0
-for grade in grade:
-  sum = sum + grade
 
-avg = sum / len(grdes)
-
-if avg >= 90:
-    letter_grade = "A"
-elif avg >= 80 and avg < 90
-    letter_grade = "B"
-elif avg > 69 and avg < 80:
-    letter_grade = "C'
-elif avg <= 69 and avg >= 65:
-    letter_grade = "D"
-elif:
-    letter_grade = "F"
-
+def find_grades(grades):
+    if avg>=90:
+        return "A"
+    elif avg>=80 and avg<=89:
+        return "B"
+    elif avg>=70 and avg<=79:
+        return "C"
+    elif avg>=60 and avg<=69:
+        return "D"
+    else:
+        return "F"
+Grade=(find_grades(grades))
+print("Grade:",Grade)
 for grade in grades:
     print("Exam: " + str(grade))
 
@@ -53,6 +50,6 @@ for grade in grades:
     print("Grade: " + letter_grade)
 
 if letter-grade is "F":
-    print "Student is failing."
+    print("Student is failing.")
 else:
-    print "Student is passing."
+    print("Student is passing.")
