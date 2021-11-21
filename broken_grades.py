4d44f6ebb592f7f4f4cc99bcbd8451cf82b28648
Ismaeil2001 <ID245@live.mdx.ac.uk>
2021-11-21 18:49:00 +0400

    fixed average error
    fixed the errors for calculating the grades average

--- broken_grades.py
+++ broken_grades.py
@@ -21,11 +21,13 @@
 
 exam_one = int(input("Input exam grade one: "))
 
-exam_two = input("Input exam grade two: "))
+exam_two = int(input("Input exam grade two: "))
 
-exam_3 = str(input("Input exam grade three: "))
+exam_three = int(input("Input exam grade three: "))
 
-grades = [exam_one exam_two exam_three]
+grades=(exam_one + exam_two + exam_three)
+avg=grades/3
+print("Average:",avg)
 sum = 0
 for grade in grade:
   sum = sum + grade
