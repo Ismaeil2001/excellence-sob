75e6532e7f830bc115f6015239dbfd13a58c0ebc
Ismaeil2001 <ID245@live.mdx.ac.uk>
2021-11-21 19:04:55 +0400

    fixed the elif error
    fixed the 'elif' error and the brackets error. I also added a print output in 'else'

--- broken_pirate.py
+++ broken_pirate.py
@@ -1,5 +1,7 @@
 greeting = input("Hello, possible pirate! What's the password?)
-if greeting in ["Arrr!"):
+if greeting in ("Arrr!"):
 	print("Go away, pirate.")
-elif
-print("Greetings, hater of pirates!")
+elif greeting in ("I h8 pirates"):
+        print("Greetings, hater of pirates!")
+else:
+        print("greetings Man/Woman who's not a hater or a pirate")
